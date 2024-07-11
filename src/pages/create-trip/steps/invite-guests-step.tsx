a3e6e8948c09ccc28015cc64112b636318c0300f
Diogo Padoveis <diogopadoveis@gmail.com>
2024-07-11 19:28:35 -0300

    Organizing trip details page and implementing the button component

--- src/pages/create-trip/steps/invite-guests-step.tsx
+++ src/pages/create-trip/steps/invite-guests-step.tsx
@@ -1,32 +1,34 @@
-import { ArrowRight, UserRoundPlus } from "lucide-react";
+import { UserRoundPlus, ArrowRight } from "lucide-react";
+import { Button } from "../../../components/button";
 
 interface InviteGuestsStepProps {
-    openGuestsModal: () => void;
-    openConfirmTripModal: () => void
-    emailsToInvite: string[]
+  openGuestsModal: () => void;
+  openConfirmTripModal: () => void;
+  emailsToInvite: string[]
 }
+
 export function InviteGuestsStep({
-    emailsToInvite,
-    openConfirmTripModal,
-    openGuestsModal
+  emailsToInvite,
+  openConfirmTripModal,
+  openGuestsModal
 }: InviteGuestsStepProps) {
-    return (
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-            <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1 text-left">
-                <UserRoundPlus className="size-5 text-zinc-400" />
-                {emailsToInvite.length > 0 ? (
-                    <span className="text-zinc-100 text-lg flex-1">{emailsToInvite.length} pessoa(s) convidada(s)</span>
-                ) : (
-                    <span className="text-zinc-400 text-lg flex-1">Quem estará na viagem?</span>
-                )}
-            </button>
+  return (
+    <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+      <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1 text-left">
+        <UserRoundPlus className="size-5 text-zinc-400" />
+        {emailsToInvite.length > 0 ? (
+          <span className="text-zinc-100 text-lg flex-1">{emailsToInvite.length} pessoa(s) convidada(s)</span>
+        ) : (
+          <span className="text-zinc-400 text-lg flex-1">Quem estará na viagem?</span>
+        )}
+      </button>
 
-            <div className="w-px h-6 bg-zinc-800" />
+      <div className="w-px h-6 bg-zinc-800" />
 
-            <button onClick={openConfirmTripModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
-                Confirmar viagem
-                <ArrowRight className="size-5" />
-            </button>
-        </div>
-    )
+      <Button onClick={openConfirmTripModal}>
+        Confirmar viagem
+        <ArrowRight className="size-5" />
+      </Button>
+    </div>
+  )
 }
