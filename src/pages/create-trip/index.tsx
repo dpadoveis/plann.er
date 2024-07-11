5ade6be140603ab652da2b443090be1f2a4a6807
Diogo Padoveis <diogopadoveis@gmail.com>
2024-07-11 10:27:34 -0300

    Separate confirm trip modal function

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,7 +1,8 @@
-import { ArrowRight, AtSign, Calendar, MapPin, Plus, Settings2, UserRoundPlus, X, User } from "lucide-react";
+import { ArrowRight, Calendar, MapPin, Settings2, UserRoundPlus } from "lucide-react";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { InviteGuestsModal } from "./invite-guests-modal";
+import { ConfirmTripModal } from "./confirm-trip-modal";
 
 export function CreateTripPage() {
     const navigate = useNavigate()
@@ -66,7 +67,9 @@ export function CreateTripPage() {
         setEmailsToInvite(newEmailList)
     }
 
-    function createTrip() {
+    function createTrip(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+
         navigate('/trips/123')
     }
 
@@ -153,48 +156,10 @@ export function CreateTripPage() {
                 />
             )}
             {isConfirmTripModalOpen && (
-                <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-                    <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
-                        <div className="space-y-2">
-                            <div className="flex items-center justify-between">
-                                <h2 className="font-lg font-semibold">Confirmar criação da viagem</h2>
-                                <button>
-                                    <X className="size-5 text-zinc-400" onClick={closeConfirmTripModal} />
-                                </button>
-                            </div>
-
-                            <p className="text-sm text-zinc-400">
-                                Para concluir a criação da viagem para <span className="font-semibold text-zinc-100">Florianópolis, Brasil</span> nas datas de <span className="font-semibold text-zinc-100">16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
-                            </p>
-                        </div>
-
-                        <form onSubmit={addNewEmailToInvite} className=" space-y-3">
-                            <div className="h-14 py-2.5 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-                                <User className="text-zinc-400 size-5" />
-                                <input
-                                    type="text"
-                                    name="name"
-                                    placeholder="Seu nome completo"
-                                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                                />
-                            </div>
-
-                            <div className="h-14 py-2.5 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-                                <User className="text-zinc-400 size-5" />
-                                <input
-                                    type="email"
-                                    name="email"
-                                    placeholder="Seu e-mail pessoal"
-                                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                                />
-                            </div>
-
-                            <button onClick={createTrip} type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
-                                Confirmar criação da viagem
-                            </button>
-                        </form>
-                    </div>
-                </div>
+                <ConfirmTripModal
+                    closeConfirmTripModal={closeConfirmTripModal}
+                    createTrip={createTrip}
+                />
             )}
         </div>
     );
