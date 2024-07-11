e04b77ba3ae1735ca9285a31947ae8de8588fa0c
Diogo Padoveis <diogopadoveis@gmail.com>
2024-07-11 10:41:56 -0300

    Separate create trip steps

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,8 +1,9 @@
-import { ArrowRight, Calendar, MapPin, Settings2, UserRoundPlus } from "lucide-react";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { InviteGuestsModal } from "./invite-guests-modal";
 import { ConfirmTripModal } from "./confirm-trip-modal";
+import { DestinationAndDateStep } from "./steps/destination-and-date-step";
+import { InviteGuestsStep } from "./steps/invite-guests-step";
 
 export function CreateTripPage() {
     const navigate = useNavigate()
@@ -84,60 +85,18 @@ export function CreateTripPage() {
                 </div>
 
                 <div className="space-y-4">
-                    <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-                        <div className="flex items-center gap-2 flex-1">
-                            <MapPin className="size-5 text-zinc-400" />
-                            <input
-                                disabled={isGuestsInputOpen}
-                                type="text"
-                                placeholder="Para onde você vai?"
-                                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                            />
-                        </div>
-
-                        <div className="flex items-center gap-2">
-                            <Calendar className="size-5 text-zinc-400" />
-                            <input
-                                disabled={isGuestsInputOpen}
-                                type="text"
-                                placeholder="Quando?"
-                                className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
-                            />
-                        </div>
-
-                        <div className="w-px h-6 bg-zinc-800" />
-
-                        {isGuestsInputOpen ? (
-                            <button onClick={closeGuestsInput} className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
-                                Alterar local/data
-                                <Settings2 className="size-5" />
-                            </button>
-                        ) : (
-                            <button onClick={openGuestsInput} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
-                                Continuar
-                                <ArrowRight className="size-5" />
-                            </button>
-                        )}
-                    </div>
+                    <DestinationAndDateStep
+                        closeGuestsInput={closeGuestsInput}
+                        isGuestsInputOpen={isGuestsInputOpen}
+                        openGuestsInput={openGuestsInput}
+                    />
 
                     {isGuestsInputOpen && (
-                        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-                            <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1 text-left">
-                                <UserRoundPlus className="size-5 text-zinc-400" />
-                                {emailsToInvite.length > 0 ? (
-                                    <span className="text-zinc-100 text-lg flex-1">{emailsToInvite.length} pessoa(s) convidada(s)</span>
-                                ) : (
-                                    <span className="text-zinc-400 text-lg flex-1">Quem estará na viagem?</span>
-                                )}
-                            </button>
-
-                            <div className="w-px h-6 bg-zinc-800" />
-
-                            <button onClick={openConfirmTripModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
-                                Confirmar viagem
-                                <ArrowRight className="size-5" />
-                            </button>
-                        </div>
+                        <InviteGuestsStep
+                            emailsToInvite={emailsToInvite}
+                            openConfirmTripModal={openConfirmTripModal}
+                            openGuestsModal={openGuestsModal}
+                        />
                     )}
                 </div>
 
