9a7b1a819e6b22c8fa90ef5b092e2276f1653680
Diogo Padoveis <diogopadoveis@gmail.com>
2025-02-13 10:31:16 -0300

    corrigindo roteamento

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './',
+  base: "/plann.er/", // Caminho do repositório no GitHub Pages
   build: {
-    outDir: 'dist',
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
   },
 });
